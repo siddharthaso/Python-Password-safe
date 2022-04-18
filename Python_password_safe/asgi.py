3c0ee19c8ec4af9e140f92920f31a012a7b91599
siddharthaso <siddasodariya10127@gmail.com>
2022-04-18 19:07:03 +0530

    v1

---
+++ Python_password_safe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Python_password_safe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Python_password_safe.settings')
+
+application = get_asgi_application()
